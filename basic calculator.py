2eabc01230f33e290ee7e4ea414f394b484f6570
Taropafadzwa Chitumwa <mandisii@icloud.com>
2024-09-27 07:48:30 +0200

    While loop included

--- basic calculator.py
+++ basic calculator.py
@@ -12,23 +12,25 @@ def divide(a, b):
           return a/b
      else:
           print('Cannot divide by 0!')
+while True:
+     num1 = float(input("Enter the first number: "))
+     operator = input("Select an operator +,-,*,/ or q to quit the program: ")
+     if operator == "q":
+          break
+     num2 = float(input("Enter the second number: "))
 
-num1 = int(input("Enter the first number: "))
-operator = input("Select an operator +,-,*,/: ")
-num2 = int(input("Enter the second number: "))
-
-if operator == "+":
-    sum = add(num1, num2)
-    print(f"The sum is {sum}")
-elif operator == "-":
-     difference = subtract(num1, num2)
-     print(f"The difference is {difference}")
-elif  operator == "*":
-     product = multiply(num1, num2)
-     print(f"The product is {product}")
-elif  operator == "/":
-     quotient = divide(num1, num2)
-     if quotient != None:
-          print(f"The quotient is {quotient}")
-else:
-     print("Invalid option")
+     if operator == "+":
+          sum = add(num1, num2)
+          print(f"The sum is {sum}")
+     elif operator == "-":
+          difference = subtract(num1, num2)
+          print(f"The difference is {difference}")
+     elif  operator == "*":
+          product = multiply(num1, num2)
+          print(f"The product is {product}")
+     elif  operator == "/":
+          quotient = divide(num1, num2)
+          if quotient != None:
+               print(f"The quotient is {quotient}")
+     else:
+          print("Invalid option")
